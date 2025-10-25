9575f0c638fc46fc3002edfaee5a5af9ffab521a
abdelrahman <abdelrahmannn202@gmail.com>
2025-10-25 23:55:07 +0300

    Configure Express server with songs route

--- src/server.js
+++ src/server.js
@@ -1,11 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 
+import songRouter from "./routes/songsRoute.js";
+
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api/song", songRouter);
 
 const port = process.env.PORT || 3000;
 
