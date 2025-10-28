0ad45e59e114c00356fc505fdae2885803c5feb4
abdelrahman <abdelrahmannn202@gmail.com>
2025-10-28 10:56:42 +0300

    feat(spectrogram): add Hamming window to reduce spectral leakage

--- src/algorithms/spectrogram.js
+++ src/algorithms/spectrogram.js
@@ -21,11 +21,25 @@ function getMagnitude({ re, im }) {
   return Math.sqrt(re ** 2 + im ** 2);
 }
 
+function applyingHamming(samples) {
+  const N = samples.length;
+  const windowed = new Float32Array(N);
+
+  for (let n = 0; n < N; n++) {
+    const w = 54 - 46 * Math.cos((2 * Math.PI * n) / (n - 1));
+    windowed[n] = samples[n] * w;
+  }
+
+  return windowed;
+}
+
 function spectrogram(samples) {
   let samplesWindows = slidingWindow(samples);
 
   let spectrogram_list = [];
   for (let window of samplesWindows) {
+    window = applyingHamming(window);
+
     const spectrum = fft(window);
 
     const magnitudes = [];
