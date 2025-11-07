055b9c9062107901d62636d03f73281665b88269
abdelrahman <abdelrahmannn202@gmail.com>
2025-11-07 11:49:59 +0200

    feat: integrate fingerprinting into song upload controller

--- src/controllers/songController.js
+++ src/controllers/songController.js
@@ -2,7 +2,8 @@ import asyncHandler from "express-async-handler";
 
 import { decodeAudioBuffer } from "../utils/audioDecoder.js";
 import spectrogram from "../algorithms/dsp/spectrogram.js";
-import detectPeaks from "../algorithms/fingerprinting/peakDetection.js";
+import detectPeaks from "../algorithms/fingerprint/peakDetection.js";
+import fingerprinting from "../algorithms/fingerprint/fingerPrinting.js";
 
 export const uploadSongController = asyncHandler(async (req, res) => {
   try {
@@ -16,7 +17,8 @@ export const uploadSongController = asyncHandler(async (req, res) => {
     const spec = spectrogram(samples);
 
     const peaks = detectPeaks(spec);
-
+    const fingerprint = fingerprinting(peaks, 1)
+    console.log(fingerprint[1])
     res.status(200).json({
       message: "Audio decoded successfully",
       sampleCount: samples.length,
